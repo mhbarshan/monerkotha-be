eb504d0a43944e4cc8f883a78c9b90c8ba689173
mhbarshan <64357462+mhbarshan@users.noreply.github.com>
2024-07-07 16:25:24 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 import mysql from "mysql";
-import * as dotenv from 'dotenv' 
-dotenv.config()
 
 export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database:process.env.DB_NAME,
+  host: "bnzvj8lugdkgwzkhrymq-mysql.services.clever-cloud.com",
+  user: "uavsc8p6rdomt0rc",
+  password: "aV474g09vIY6dCWLCT26",
+  database: "bnzvj8lugdkgwzkhrymq",
+  port: "3306",
 });
