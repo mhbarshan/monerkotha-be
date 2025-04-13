a19bcb83baf7a668b3d47b4d43ac77b254bb6ac4
mhbarshan <64357462+mhbarshan@users.noreply.github.com>
2025-04-14 05:01:17 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: "sql12.freesqldatabase.com",
-  user: "sql12772968",
-  password: "ebP5nF9In9",
-  database: "sql12772968",
+  host: "bnzvj8lugdkgwzkhrymq-mysql.services.clever-cloud.com",
+  user: "uavsc8p6rdomt0rc",
+  password: "aV474g09vIY6dCWLCT26",
+  database: "bnzvj8lugdkgwzkhrymq",
   port: "3306",
 });
 
-if(db) console.log("Connected");
+if(db) console.log("db Connected");
 else console.log("Connecting..")
